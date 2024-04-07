6c32aa0d894235f1c4de0efae8c32e01389ef57a
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-04-07 17:01:25 -0300

    feat: função conectar DB

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-	
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000",router))
+
 }
